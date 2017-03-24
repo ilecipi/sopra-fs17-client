63c47f3923e386ee5d1709e49158439a25de7898
SilvoSposetti <silvo.sposetti@uzh.ch>
2017-03-24 11:53:45 +0100

    Lobby: colors for different game and user status

---
+++ dist/out-tsc/app/shared/models/game.js
@@ -0,0 +1,6 @@
+export var Game = (function () {
+    function Game() {
+    }
+    return Game;
+}());
+//# sourceMappingURL=C:/Users/Silvo/Dropbox/UZH/FS17/SOPRA/Git repository/sopra-fs17-group11-client/src/app/shared/models/game.js.map
---
+++ dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,6 @@
+export var environment = {
+    envName: 'prod',
+    production: true,
+    apiUrl: 'https://sopra-fs17-group11.herokuapp.com'
+};
+//# sourceMappingURL=C:/Users/Silvo/Dropbox/UZH/FS17/SOPRA/Git repository/sopra-fs17-group11-client/src/environments/environment.prod.js.map
