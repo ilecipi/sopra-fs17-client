e3c82d797263382447ce0d8e8efe48cc46ff4105
SilvoSposetti <silvo.sposetti@uzh.ch>
2017-03-25 16:43:46 +0100

    replaced floats in css and added waiting room

--- dist/out-tsc/main.js
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
-if (environment.production) {
-    enableProdMode();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=C:/Users/Silvo/Dropbox/UZH/FS17/SOPRA/Git repository/sopra-fs17-group11-client/src/main.js.map
