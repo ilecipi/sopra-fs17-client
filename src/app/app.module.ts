6f8cfacc6c2b7d4eaec7c6dc15f7fcf4150fcd14
ryublue <sophy.chhong@hispeed.ch>
2017-03-30 18:56:33 +0200

    necessary commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
-import {DragulaModule} from "ng2-dragula";
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from "./login/login.component";
@@ -40,7 +39,6 @@ import {AuthenticationService} from "./shared/services/authentication.service";
         FormsModule,
         HttpModule,
         JsonpModule,
-        DragulaModule,
         routing
     ],
     providers: [AuthenticationService, AuthGuardService, UserService, GameService],
