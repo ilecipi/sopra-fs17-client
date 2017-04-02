b30ff660ac6ff493cb198ee558321345de938048
ryublue <sophy.chhong@hispeed.ch>
2017-04-02 22:47:46 +0200

    can sail ships to all siteboards

--- src/app/harbour/harbour.component.ts
+++ src/app/harbour/harbour.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ShipService} from "../shared/services/ship.service";
 
-import {Ng2DragDropModule} from "ng2-drag-drop";
+import {Ship} from "../shared/models/ship";
+
 
 @Component({
   selector: 'harbour',
@@ -11,36 +12,52 @@ import {Ng2DragDropModule} from "ng2-drag-drop";
 
 export class HarbourComponent implements OnInit {
   round: number;
+  //ships: Ship[];
 
-  //Drag and drop ship
-  items = [
-    {name: "Ship1", type: "Ship"},
-    {name: "Ship2", type: "Ship"},
-    {name: "Ship3", type: "Ship"},
-    {name: "Ship4", type: "Ship"},];
-  droppedItems = [];
+  //default ships for testing
+  ships: Ship[] = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
 
-  onItemDrop(e: any) {
-    // Get the dropped data here
-    this.droppedItems.push(e.dragData);
-    this.removeItem(e.dragData, this.items);
-  }
-
-  removeItem(item: any, list: Array<any>) {
-    let index = list.map((e) => {
-      return e.name
-    }).indexOf(item.name);
-    list.splice(index, 1);
-  }
-
-
-
+  market = [];
+  pyramid= [];
+  temple= [];
+  burialchamber= [];
+  obelisk= [];
 
   constructor(private shipService: ShipService) { }
 
   ngOnInit() {
-    this.round++;
-    this.shipService.getShips(this.round);
+    this.round = 1;
+
+    //get ships for first round
+    /*this.shipService.getShips(this.round)
+       .subscribe(ships => {this.ships = ships;
+        });*/
   }
+
+  //dock ship on market
+  onMarketDrop(e: any) {
+    this.shipService.onDrop(e, this.ships, this.market);
+  }
+
+  //dock ship on pyramid
+  onPyramidDrop(e: any) {
+    this.shipService.onDrop(e, this.ships, this.pyramid);
+  }
+
+  //dock ship on temple
+  onTempleDrop(e: any) {
+    this.shipService.onDrop(e, this.ships, this.temple);
+  }
+
+  //dock ship on burial chamber
+  onBurialchamberDrop(e: any) {
+    this.shipService.onDrop(e, this.ships, this.burialchamber);
+  }
+
+  //dock ship on obelisk
+  onObeliskDrop(e: any) {
+    this.shipService.onDrop(e, this.ships, this.obelisk);
+  }
+
 
 }
