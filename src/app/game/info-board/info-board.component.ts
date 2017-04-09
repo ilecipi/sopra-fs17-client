b5d15e01ee101e42b599190d214f5e38d3e0776d
SilvoSposetti <silvo.sposetti@uzh.ch>
2017-04-09 18:18:19 +0200

    scoring track board can show points
    This was such a pain...

--- src/app/game/info-board/info-board.component.ts
+++ src/app/game/info-board/info-board.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Game} from '../../shared/models/game';
+import {Points} from '../../shared/models/points';
 import {Observable} from "rxjs/Rx";
 
 @Component({
@@ -11,7 +12,10 @@ export class InfoBoardComponent implements OnInit {
 
     @Input()
     private currentGame: Game;
+
     private counter = 0;
+    private showSnake: boolean;
+    // styles needed to color cells in scoring track board.
     style1 = document.createElement('style');
     style2 = document.createElement('style');
     style3 = document.createElement('style');
@@ -25,16 +29,9 @@ export class InfoBoardComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.style1.innerHTML = '.order0 {background: linear-gradient(45deg,#ffffff,#000000)}';
-        this.style2.innerHTML = '.order1 {background: linear-gradient(45deg,#000000,#ffffff)}';
-        this.style3.innerHTML = '.order1 {background: linear-gradient(45deg,#000000,#ffffff)}';
-        this.style4.innerHTML = '.order1 {background: linear-gradient(45deg,#000000,#ffffff)}';
-
-
-        document.getElementsByTagName('game')[0].appendChild(this.style1);
-        document.getElementsByTagName('game')[0].appendChild(this.style2);
-        document.getElementsByTagName('game')[0].appendChild(this.style3);
-        document.getElementsByTagName('game')[0].appendChild(this.style4);
+        this.showSnake = false;
+        this.initStyleChildren();
+        this.addStyleChildren();
         this.pollColors();
     }
 
@@ -59,49 +56,178 @@ export class InfoBoardComponent implements OnInit {
 
     }
 
-    pollColors() {
-        Observable.interval(100).subscribe(x => {
-            this.getColors();
+    pollColors(): void {
+        this.styleSubscription = Observable.interval(100).subscribe(x => {
+            if (this.showSnake) {
+                this.setTrackBoardSnake();
+            }
+            else {
+                this.setTrackBoardCells();
+
+            }
+
         })
 
     }
 
-    getColors(): void {
-        if (this.counter % 2 == 0) {
-            document.getElementsByTagName('game')[0].removeChild(this.style2);
-            document.getElementsByTagName('game')[0].removeChild(this.style1);
-            document.getElementsByTagName('game')[0].removeChild(this.style3);
-            document.getElementsByTagName('game')[0].removeChild(this.style4);
-            this.style1.innerHTML = '.order' + this.counter % 40 + ' {background: #000000}';
-            this.style2.innerHTML = '.order' + (this.counter + 1) % 40 + ' {background: #ffffff}';
-            this.style3.innerHTML = '.order' + (this.counter + 2) % 40 + ' {background: #000000}';
-            this.style4.innerHTML = '.order' + (this.counter + 3) % 40 + ' {background: #ffffff}';
 
-            document.getElementsByTagName('game')[0].appendChild(this.style1);
-            document.getElementsByTagName('game')[0].appendChild(this.style2);
-            document.getElementsByTagName('game')[0].appendChild(this.style3);
-            document.getElementsByTagName('game')[0].appendChild(this.style4);
+    setTrackBoardCells(): void {
+        //Each iteration of the function requires the old style to be removed, changed, and then added again
+        this.removeStyleChildren();
 
-
+        let pointsArray = [];
+        let points = this.currentGame.points;
+        // push on the pointsArray every color present in currentGame.points with his respective points
+        if (points.brown != undefined) {
+            pointsArray.push(['brown', points.brown % 40, 'var(--brownStone)']);
         }
-        else {
-            document.getElementsByTagName('game')[0].removeChild(this.style2);
-            document.getElementsByTagName('game')[0].removeChild(this.style1);
-            document.getElementsByTagName('game')[0].removeChild(this.style3);
-            document.getElementsByTagName('game')[0].removeChild(this.style4);
-            this.style1.innerHTML = '.order' + this.counter % 40 + ' {background: #ffffff}';
-            this.style2.innerHTML = '.order' + (this.counter + 1) % 40 + ' {background: #000000}';
-            this.style3.innerHTML = '.order' + (this.counter + 2) % 40 + ' {background: #ffffff}';
-            this.style4.innerHTML = '.order' + (this.counter + 3) % 40 + ' {background: #000000}';
-            document.getElementsByTagName('game')[0].appendChild(this.style1);
-            document.getElementsByTagName('game')[0].appendChild(this.style2);
-            document.getElementsByTagName('game')[0].appendChild(this.style3);
-            document.getElementsByTagName('game')[0].appendChild(this.style4);
-
-
+        if (points.black != undefined) {
+            pointsArray.push(['black', points.black % 40, 'var(--blackStone)']);
         }
-        this.counter++;
+        if (points.white != undefined) {
+            pointsArray.push(['white', points.white % 40, 'var(--whiteStone)']);
+        }
+        if (points.grey != undefined) {
+            pointsArray.push(['grey', points.grey % 40, 'var(--greyStone)']);
+        }
+
+        //important: these two list are "connected" with indexes!
+        let distinctScore = []; // is going to contain all distincts scores
+        let counts = [];    // is going to contain the amount of time each score presents itself
+        for (let i = 0; i < pointsArray.length; i++) {
+            if (distinctScore.some(x => x == pointsArray[i][1])) {
+                counts[distinctScore.indexOf(pointsArray[i][1])] += 1;
+            } else {
+                distinctScore.push(pointsArray[i][1]);
+                counts.push(1);
+            }
+        }
+
+        // now need to construct boardStatus such that every row of boardStatus has:
+        // first element: the actual score number
+        // second element: a list containing the strings that need to be used for that color (e.g. [var(--blackStone),var(--whiteStone)])
+
+        let boardStatus = []; //is going to contain every color of the game
+        for (let i = 0; i < distinctScore.length; i++) {
+            let element = [];// this is one row in boardStatus
+            element.push(distinctScore[i]);
+            let colors = [];
+            for (let j = 0; j < pointsArray.length; j++) {
+                if (distinctScore[i] == pointsArray[j][1]) {
+                    colors.push(pointsArray[j][2]); //add every string to be used with that specific score amount
+                }
+            }
+            element.push(colors);
+            boardStatus.push(element);
+        }
+
+        // now boardStatus has a structure like this:
+        // [
+        //     [
+        //         2,
+        //         [
+        //             var(--blackStone),
+        //             var(--whiteStone)
+        //         ]
+        //     ],
+        //     [
+        //         3,
+        //          [
+        //             var(--brownStone),
+        //             var (--greyStone)
+        //         ]
+        //     ]
+        // ]
+
+
+        // now need to create the style to apply:
+
+        //  example style: background: linear-gradient(to right, #ffffff,#ffffff 49.9%, #000000 50.1%, #000000);
+        let styleCounter = 1;
+        for (let i = 0; i < boardStatus.length; i++) {
+            let styleString: string;
+            if (styleCounter == 1) {
+                styleString = this.setStyleString(boardStatus, i);
+                this.style1.innerHTML = styleString;
+            }
+            else if (styleCounter == 2) {
+                styleString = this.setStyleString(boardStatus, i);
+                this.style2.innerHTML = styleString;
+            }
+            else if (styleCounter == 3) {
+                styleString = this.setStyleString(boardStatus, i);
+                this.style3.innerHTML = styleString;
+            }
+            else {
+                styleString = this.setStyleString(boardStatus, i);
+                this.style4.innerHTML = styleString;
+            }
+            styleCounter++;
+        }
+        this.addStyleChildren(); // adds the previously generated styles
+    }
+
+    removeStyleChildren(): void {
+        document.getElementsByTagName('info-board')[0].removeChild(this.style1);
+        document.getElementsByTagName('info-board')[0].removeChild(this.style2);
+        document.getElementsByTagName('info-board')[0].removeChild(this.style3);
+        document.getElementsByTagName('info-board')[0].removeChild(this.style4);
+    }
+
+    addStyleChildren(): void {
+        document.getElementsByTagName('info-board')[0].appendChild(this.style1);
+        document.getElementsByTagName('info-board')[0].appendChild(this.style2);
+        document.getElementsByTagName('info-board')[0].appendChild(this.style3);
+        document.getElementsByTagName('info-board')[0].appendChild(this.style4);
     }
 
 
+    initStyleChildren(): void {
+        this.style1.innerHTML = '';
+        this.style2.innerHTML = '';
+        this.style3.innerHTML = '';
+        this.style4.innerHTML = '';
+    }
+
+    // snake easter egg
+    setTrackBoardSnake(): void {
+        this.removeStyleChildren();
+
+        this.style1.innerHTML = '.order' + this.counter + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 1) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 2) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 3) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 4) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 5) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 6) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 30%, #648880 40%, rgba(0,0,0,0) );}' +
+            '.order' + (this.counter + 7) % 40 + ' {background: radial-gradient(circle,#293f50, #293f50 50%, #648880 60%, rgba(0,0,0,0) );}';
+        this.style2.innerHTML = '';
+        this.style3.innerHTML = '';
+        this.style4.innerHTML = '';
+
+        this.addStyleChildren();
+
+        this.counter++;
+        this.counter = this.counter % 40;
+    }
+
+    setStyleString(boardStatus: any, i: number): string {// this was such a pain, believe me I coded it... ok with some help.
+
+        let styleString = '.order' + boardStatus[i][0] + ' { background: linear-gradient( to right, '; //set beginning of styleString
+
+        for (let j = 0; j < boardStatus[i][1].length; j++) {    // for each single color assigned to a score in boardStatus
+            let percent = 100 / boardStatus[i][1].length;       // percent that that color occupies in the square based on how many colors are concurrently there
+            styleString += boardStatus[i][1][j] + ' ' + percent * (j) + '%, ' +     // first stop of the color
+                boardStatus[i][1][j] + ' ' + (percent * (j + 1) - 0.1) + '%, ';     // second stop of the color
+        }
+        styleString = styleString.substring(0, styleString.length - 2);     // remove last comma and space, which are not needed
+        styleString += ');}';    //close parenthesis, and voila, you have your style sir *tips hat*.
+        return styleString;
+    }
+
+    hiddenButton() {
+        this.showSnake = !this.showSnake;
+    }
+
 }
+
