1bdb406c5b9d5cfc302caa46aab7318134e738ec
SilvoSposetti <silvo.sposetti@uzh.ch>
2017-04-12 18:24:43 +0200

    can display cards in current player area

--- src/app/game/current-player/current-player.component.ts
+++ src/app/game/current-player/current-player.component.ts
@@ -5,17 +5,21 @@ import {UserService} from '../../shared/services/user.service';
 import {MoveService} from '../../shared/services/move.service';
 import {ShipService} from '../../shared/services/ship.service';
 import {Game} from "../../shared/models/game";
+import {Card} from '../../shared/models/card';
 
 @Component({
     selector: 'current-player',
     templateUrl: './current-player.component.html',
-    styleUrls: ['./current-player.component.css']
+    styleUrls: ['./current-player.component.css'],
 })
 export class CurrentPlayerComponent implements OnInit {
     @Input()
     currentUser: User;
     @Input()
     currentGame: Game; //need to know the current game for sending the addStones move request
+
+
+    currentCards: Card[] = [];
 
     constructor(private gameService: GameService,
                 private userService: UserService,
@@ -24,6 +28,20 @@ export class CurrentPlayerComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.currentCards.push(new Card('BURIAL_CHAMBER_DECORATION'));
+        this.currentCards.push(new Card('PYRAMID_DECORATION'));
+        this.currentCards.push(new Card('HAMMER'));
+        this.currentCards.push(new Card('SAIL'));
+        this.currentCards.push(new Card('SARCOPHAGUS'));
+        this.currentCards.push(new Card('ENTRANCE'));
+        this.currentCards.push(new Card('OBELISK_DECORATION'));
+        this.currentCards.push(new Card('LEVER'));
+        this.currentCards.push(new Card('TEMPLE_DECORATION'));
+        this.currentCards.push(new Card('STATUE'));
+        this.currentCards.push(new Card('PAVED_PATH'));
+        this.currentCards.push(new Card('CHISEL'));
+
+        //15 cards added;
     }
 
     triggerAddStones() {
--- src/app/shared/models/card.ts
+++ src/app/shared/models/card.ts
@@ -1,8 +1,136 @@
 export class Card {
-    id: number;
+    name: string;
     title: string;
     description: string;
-    // image: string; //path to image maybe?
+    color: string; //color is either: 'blue', 'green', 'red' or 'purple'
+    imagePath: string; //path to the image
 
 
+    // Build the imagePath on the component and assign it to a variable, then:
+    // <img [src]="urlVariable" /> in the html file
+
+    // Then to put the cards style into the appropriate component:
+    // @Component({
+    //     selector: 'some-component',
+    //     template: '<div></div>',
+    //     styleUrls: [
+    //         'http://example.com/external.css',
+    //         'app/local.css'
+    //     ],
+    //     encapsulation: ViewEncapsulation.None,
+    // })
+    // export class SomeComponent {}
+
+
+
+    //TODO: define or remove image paths of the cards, depends on what we want to do.
+
+    constructor(name: string) {
+        switch (name) {
+
+            //GREEN CARDS: *********************************************************************************************
+            case 'BURIAL_CHAMBER_DECORATION': {
+                this.name = 'BURIAL_CHAMBER_DECORATION';
+                this.title = 'BURIAL CHAMBER DECORATION';
+                this.color = "green";
+                this.description = 'At End of Game: Earn 1 point per 3 stones in the burial chamber. (your own + others\' stones).';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'PYRAMID_DECORATION': {
+                this.name = 'PYRAMID_DECORATION';
+                this.title = 'PYRAMID DECORATION';
+                this.color = "green";
+                this.description = 'At End of Game: Earn 1 point per 3 stones in the pyramid. (your own + others\' stones).';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'TEMPLE_DECORATION': {
+                this.name = 'TEMPLE_DECORATION';
+                this.title = 'TEMPLE DECORATION';
+                this.color = "green";
+                this.description = 'At End of Game: Earn 1 point per 3 stones in the temple. (your own + others\' stones).';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'OBELISK_DECORATION': {
+                this.name = 'OBELISK_DECORATION';
+                this.title = 'OBELISK DECORATION';
+                this.color = "green";
+                this.description = 'At End of Game: Earn 1 point per 3 stones in the obelisk. (your own + others\' stones).';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+
+            //BLUE CARDS: **********************************************************************************************
+            case 'HAMMER': {
+                this.name = 'HAMMER';
+                this.title = 'HAMMER';
+                this.color = "blue";
+                this.description = '1x Action: Excavate 3 stones from the quarry to your supply sled AND place 1 stone on 1 ship.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'SAIL': {
+                this.name = 'SAIL';
+                this.title = 'SAIL';
+                this.color = "blue";
+                this.description = '1x Action: Place 1 stone on 1 ship AND sail the ship to a site.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'LEVER': {
+                this.name = 'LEVER';
+                this.title = 'LEVER';
+                this.color = "blue";
+                this.description = '1x Action: Sail 1 ship to a site. Decide for yourself the order in which to unload the stones.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'CHISEL': {
+                this.name = 'CHISEL';
+                this.title = 'CHISEL';
+                this.color = "blue";
+                this.description = '1x Action: Place 2 stones on 1 ship OR place 1 stone on each of 2 ships.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+
+            //RED CARDS: ***********************************************************************************************
+            case 'SARCOPHAGUS': {
+                this.name = 'SARCOPHAGUS';
+                this.title = 'SARCOPHAGUS';
+                this.color = "red";
+                this.description = 'Immediately: Place 1 stone from the quarry in the burial chamber.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'ENTRANCE': {
+                this.name = 'ENTRANCE';
+                this.title = 'ENTRANCE';
+                this.color = "red";
+                this.description = 'Immediately; Place 1 stone from the quarry in the pyramid.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+            case 'PAVED_PATH': {
+                this.name = 'PAVED_PATH';
+                this.title = 'PAVED PATH';
+                this.color = "red";
+                this.description = 'Immediately: Place 1 stone from the quarry on your obelisk.';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+
+            //PURPLE CARDS: ********************************************************************************************
+            case 'STATUE': {
+                this.name = 'STATUE';
+                this.title = 'STATUE';
+                this.color = "purple";
+                this.description = 'At End of Game: Number of statues';
+                this.imagePath = 'not yet defined';
+                break;
+            }
+        }
+    }
 }
