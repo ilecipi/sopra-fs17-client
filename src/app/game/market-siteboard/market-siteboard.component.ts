dd2877a10b3f0f881dca3f9774a1201801e7c1b6
SilvoSposetti <silvo.sposetti@uzh.ch>
2017-04-14 23:55:35 +0200

    hover-boxes for siteboard-info
    plus market re-layouting

--- src/app/game/market-siteboard/market-siteboard.component.ts
+++ src/app/game/market-siteboard/market-siteboard.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Card} from "../../shared/models/card";
 
 @Component({
-  selector: 'market-siteboard',
-  templateUrl: './market-siteboard.component.html',
-  styleUrls: ['./market-siteboard.component.css']
+    selector: 'market-siteboard',
+    templateUrl: './market-siteboard.component.html',
+    styleUrls: ['./market-siteboard.component.css']
 })
 export class MarketSiteboardComponent implements OnInit {
+    cards: Card[];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.cards=[];
+        this.cards.push(new Card('SAIL'));
+        this.cards.push(new Card('SARCOPHAGUS'));
+        this.cards.push(new Card('SAIL'));
+        this.cards.push(new Card('SAIL'));
+    }
 
 }
