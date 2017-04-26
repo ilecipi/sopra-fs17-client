6df679a5c6c17e7165bfbe81e0a65f15070dae5e
Silvo Sposetti <silvosposetti@gmail.com>
2017-04-26 18:31:40 +0200

    improved notifications

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {NotificationService} from "../shared/services/notification.service";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     selector: 'notification',
@@ -8,18 +9,21 @@ import {NotificationService} from "../shared/services/notification.service";
 })
 export class NotificationComponent implements OnInit {
 
-
-    private currentNotification: string;
+    private notifications: string[];
     private notificationSubscription: any;
 
     constructor(private notificationService:NotificationService) {
     }
 
     ngOnInit() {
+        this.pollQueue();
+    }
 
-        this.notificationSubscription = this.notificationService.getCurrentNotification().subscribe((notification) => {
-            this.currentNotification = notification;
+    pollQueue(): void {
+        this.notificationSubscription = Observable.interval(100).subscribe(() => {
+            this.notifications = this.notificationService.getNotificationsQueue();
         });
     }
+
 
 }
